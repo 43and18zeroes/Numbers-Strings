e9730036a2e04529ca67817aa1c48d720a6a8ef3
Christoph <c.wagner.germany@gmail.com>
2024-05-20 13:24:32 +0200

    20May3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
-console.log(Math.pow(2, 53)-1); // 9007199254740991
+console.log(Math.pow(2, 53) - 1); // 9007199254740991
 
-console.log(Math.pow(2, 53)-1+1); // 9007199254740992
-console.log(Math.pow(2, 53)-1)+1; // 9007199254740991
+console.log(Math.pow(2, 53) - 1 + 1); // 9007199254740992
+console.log(Math.pow(2, 53) - 1) + 1; // 9007199254740991
 
 console.log(Number.MIN_SAFE_INTEGER); // -9007199254740991
 
@@ -11,15 +11,22 @@ console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
 const numbers = [1, 2, 3, 4, 5];
 
 // Die forEach Methode wird auf das Array numbers angewendet
-numbers.forEach(function(number) {
+numbers.forEach(function (number) {
   console.log(number);
 });
 
 // Definition der Callback-Funktion
 function logNumber(number) {
-    console.log(number);
-  }
-  
-  // Verwendung der Callback-Funktion
-  const numbers2 = [1, 2, 3, 4, 5];
-  numbers.forEach(logNumber);
+  console.log(number);
+}
+
+// Verwendung der Callback-Funktion
+const numbers2 = [1, 2, 3, 4, 5];
+numbers.forEach(logNumber);
+
+const numbers3 = [1, 2, 3, 4, 5];
+
+// Verwendung eines Inline-Callbacks
+numbers.forEach(function (number) {
+  console.log(number);
+});
