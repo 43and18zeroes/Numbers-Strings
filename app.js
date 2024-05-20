a393e024c132f444ffdffb78ba8b815e3eb77c20
Christoph <c.wagner.germany@gmail.com>
2024-05-20 13:19:24 +0200

    20May1

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ console.log(Math.pow(2, 53)-1)+1; // 9007199254740991
 console.log(Number.MIN_SAFE_INTEGER); // -9007199254740991
 
 console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+
+const numbers = [1, 2, 3, 4, 5];
+
+// Die forEach Methode wird auf das Array numbers angewendet
+numbers.forEach(function(number) {
+  console.log(number);
+});
