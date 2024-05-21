197d04c557d9a41e8145c4b16209f44d3455d148
Christoph <c.wagner.germany@gmail.com>
2024-05-21 15:12:27 +0200

    21May3

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
-console.log(Math.pow(2, 53)-1); // 9007199254740991
+console.log(Math.pow(2, 53) - 1); // 9007199254740991
 
-console.log(Math.pow(2, 53)-1+1); // 9007199254740992
-console.log(Math.pow(2, 53)-1)+1; // 9007199254740991
+console.log(Math.pow(2, 53) - 1 + 1); // 9007199254740992
+console.log(Math.pow(2, 53) - 1) + 1; // 9007199254740991
 
 console.log(Number.MIN_SAFE_INTEGER); // -9007199254740991
 
 console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
 
 console.log(0.2 + 0.4); // 0.6000000000000001
+console.log(0.2 + 0.4 === 0.6); // false
+console.log((5).toString(2)); // 101
+console.log(1 / 5); // 0.2
+console.log((1 / 5).toString(2)); // 0.001100110011001100110011001100110011001100110011001101
+console.log((0.2).toString(2)); // 0.001100110011001100110011001100110011001100110011001101
+console.log(1 / 3 + 1 / 3 + 1 / 3); // 1 JS rounds automatically here
+console.log((0.2).toFixed(20)); // 0.20000000000000001110
+console.log((0.2 + 0.4).toFixed(2)); // 0.60
 
-console.log(0.2 + 0.4 === 0.6);// false
-
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
