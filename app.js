0620df4ec2c415c6cc28d7dab331389f73a9185b
Christoph <c.wagner.germany@gmail.com>
2024-05-21 14:51:57 +0200

    20May9

--- app.js
+++ app.js
@@ -7,3 +7,6 @@ console.log(Math.pow(2, 53)-1)+1; // 9007199254740991
 console.log(Number.MIN_SAFE_INTEGER); // -9007199254740991
 
 console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+
+console.log(0.2 + 0.4); // 0.6000000000000001
+
